0b6347f9e544c294e754b7f49759f4eb55a16f34
noproblemo1 <d8mp@pm.me>
2024-11-26 12:54:24 +0000

    Delete app/[domain]/add video.tsx

--- app/[domain]/add video.tsx
+++
@@ -1,9 +0,0 @@
-/*
-      {/* Video embed with subtitle (No extra space between text instructions and video) */}
-/*      <div className="mt-4"> {/* Optional: Use margin-top to control spacing */}
-/*        <VideoComponent /> {/* Use the Client Component */}
-/*      </div>
-/*    </main>
-/*  )
-/*}
-/*
