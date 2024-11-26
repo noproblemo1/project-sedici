91d041f26b19a3fb2d983ab02a09b1b9295f7b6b
noproblemo1 <d8mp@pm.me>
2024-11-26 12:18:09 +0000

    Create add video.tsx

---
+++ app/[domain]/add video.tsx
@@ -0,0 +1,8 @@
+
+      {/* Video embed with subtitle (No extra space between text instructions and video) */}
+      <div className="mt-4"> {/* Optional: Use margin-top to control spacing */}
+        <VideoComponent /> {/* Use the Client Component */}
+      </div>
+    </main>
+  )
+}
