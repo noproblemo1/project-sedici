9b8381ceaac8b75ea29796978e2a8902e8c21818
noproblemo1 <d8mp@pm.me>
2024-12-07 23:40:46 +0400

    Update stage.tsx

--- components/stage.tsx
+++ components/stage.tsx
@@ -12,14 +12,14 @@ export function Stage({ number, title, disabled, last, children }: Props) {
   return (
     <section className={cn(disabled && "opacity-50")}>
       <div className="flex h-8 flex-row items-center">
-        <div className="mr-4 grid size-8 shrink-0 place-items-center rounded-full bg-slate-100 text-center dark:bg-slate-800">
+        <div className="mr-4 grid size-8 shrink-0 place-items-center rounded-full bg-muted text-center dark:bg-muted text-foreground">
           {number}
         </div>
-        <h2 className="font-semibold">{title}</h2>
+        <h2 className="font-semibold text-foreground">{title}</h2>
       </div>
       <div
         className={cn(
-          "border-l-1 ml-4 border-l py-6 pl-8",
+          "border-l-1 ml-4 border-l border-muted py-6 pl-8",  // updated border color
           last && "border-transparent"
         )}
       >
