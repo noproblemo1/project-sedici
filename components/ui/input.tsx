5837d56a007e35675d8bc316eee8eba6ade28ac4
noproblemo1 <d8mp@pm.me>
2024-12-08 15:57:00 +0400

    Update input.tsx

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,8 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         className={cn(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground",
           // Apply the ring color by default using the --foreground color
-          "ring-2 ring-[hsl(var(--foreground))] ring-offset-2",
-          "focus-visible:ring-0", // Disable the default focus-visible ring
+          "ring-2 ring-[hsl(var(--foreground))] ring-offset-2", 
           className
         )}
         ref={ref}
